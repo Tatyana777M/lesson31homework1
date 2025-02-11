9c0ec4323a80ce0d6ba0b4b4ef1489856947c296
Tatyana <Tatyanatatyana777M@gmail.com>
2025-02-11 20:37:07 +0100

    Class DHL has done

---
+++ src/DHL.java
@@ -0,0 +1,9 @@
+abstract class DHL implements MailDeliveryService{
+
+    @Override
+    public void sendMail() {
+        System.out.println("Положи письмо в конверт, поставь марку, отправь");
+
+    }
+}
+
