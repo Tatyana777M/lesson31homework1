2a67c776f17f64e2a641868b468d0d61cf8905ce
Tatyana <Tatyanatatyana777M@gmail.com>
2025-02-11 20:46:15 +0100

    Class Pigeon is created

---
+++ src/Pigeon.java
@@ -0,0 +1,7 @@
+public class Pigeon implements MailDeliveryService {
+    @Override
+    public void sendMail() {
+        System.out.println("Голубь?! Серьезно!? в двадцать первом веке?!!! Ты сумасшедший! Я улетаю");
+
+    }
+}
