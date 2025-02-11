457444bb742b9924336dbf329aaeaa2551102527
Tatyana <Tatyanatatyana777M@gmail.com>
2025-02-11 21:54:17 +0100

    Class Main has not finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        Sender sender = new Sender();
+        {
+            System.out.println("Choose a mail delivery method");
+            System.out.println("1 - DHL");
+            System.out.println("2 - Email");
+            System.out.println("3 - Pigeon");
+            int choice = scanner.nextInt();
+            scanner.close();
+
+            switch (choice) {
+                case 1:
+                    MailDeliveryService service = new DHL();
+                    break;
+                case 2:  service = new Email();
+                    break;
+                case 3:
+                    service = new Pigeon();
+                    break;
+
+            }
+        }
     }
 }
+
+
+
